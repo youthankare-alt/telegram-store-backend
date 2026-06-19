926ec3e784127fc9072672db11582c6954012463
youthankare-alt <youthankare@gmail.com>
2026-06-20 04:41:39 +0700

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  base: './', // PENTING: Mengubah penulisan path aset dari absolut menjadi relatif
   plugins: [vue()],
   server: {
     port: 3000
